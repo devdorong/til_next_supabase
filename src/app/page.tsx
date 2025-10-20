20ceb69a398a455ef5a0b50f1cddf1abff7f892f
devdorong <devdorong@gmail.com>
2025-10-20 10:59:45 +0900

    [docs] scss 설정 및 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import ButtonTest from '@/components/ButtonTest';
+import SCSSTest from '@/components/SCSSTest';
 
 export default function Home() {
   return (
-    <div className='w-screen h-screen flex items-center justify-center'>
+    <div>
       <ButtonTest />
+      <SCSSTest />
     </div>
   );
 }
