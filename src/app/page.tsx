8b51d16ac963e35f5f51baa37a2df4cb98408c76
devdorong <devdorong@gmail.com>
2025-10-20 13:18:14 +0900

    [docs] zustand 설정 및 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import ButtonTest from '@/components/ButtonTest';
+import Counter from '@/components/Counter';
 import SCSSTest from '@/components/SCSSTest';
+import UserProfile from '@/components/User';
 
 export default function Home() {
   return (
     <div>
-      <ButtonTest />
-      <SCSSTest />
+      {/* <ButtonTest /> */}
+      {/* <SCSSTest /> */}
+      <Counter />
+      <UserProfile />
     </div>
   );
 }
