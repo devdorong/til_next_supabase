cea8c806562db83be530980fd6d5bba96f008b27
devdorong <devdorong@gmail.com>
2025-10-31 11:01:50 +0900

    [docs] zustand 인증 보관 적용

--- src/stores/session.ts
+++ src/stores/session.ts
@@ -1,4 +1,54 @@
 import { create } from 'zustand';
-import { combine } from 'zustand/middleware';
+import { combine, devtools } from 'zustand/middleware';
 
-create(combine({}, () => ({})));
+// Supabase 의 인증의 타입을 정의함
+
+import type { Session } from '@supabase/supabase-js';
+
+type State = {
+  isLoading: boolean;
+  session: null | Session;
+};
+
+// 보관할 state 객체의 초기값
+const initialState: State = {
+  isLoading: false,
+  session: null,
+};
+
+// set 은 state 값 설정
+// get 은 state 값 읽기
+const useSessionStore = create(
+  devtools(
+    combine(initialState, (set, get) => ({
+      actions: {
+        setSession: (session: Session | null) => {
+          // 하고싶은일
+          set({ isLoading: true, session });
+        },
+      },
+    })),
+    { name: 'sessionStore' }
+  )
+);
+
+// session 정보
+export const useSession = () => {
+  // Selector 함수는 Store 에서 원하는 것을 선택해서 리턴한다.
+  const session = useSessionStore(store => store.session);
+  return session;
+};
+
+// loading 정보
+export const useSessionLoaded = () => {
+  // Selector 함수는 Store 에서 원하는 것을 선택해서 리턴한다.
+  const isSessionLoaded = useSessionStore(store => store.isLoading);
+  return isSessionLoaded;
+};
+
+// session 보관 액션
+export const useSetSession = () => {
+  // Selector 함수는 Store 에서 원하는 것을 선택해서 리턴한다.
+  const setSession = useSessionStore(store => store.actions.setSession);
+  return setSession;
+};
