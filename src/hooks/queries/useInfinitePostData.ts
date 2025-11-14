91ac475af6d38e7ce6cc8284da5d20f875786782
devdorong <devdorong@gmail.com>
2025-11-14 11:41:54 +0900

    [docs] 무한스크롤 적용

---
+++ src/hooks/queries/useInfinitePostData.ts
@@ -0,0 +1,24 @@
+import { fetchPosts } from '@/apis/post';
+import { QUERY_KEYS } from '@/lib/constants';
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+const PAGE_SIZE = 5;
+
+export function useInfinitePostData() {
+  return useInfiniteQuery({
+    queryKey: QUERY_KEYS.posts.list,
+    queryFn: async ({ pageParam = 0 }) => {
+      const from = pageParam * PAGE_SIZE;
+      const to = from + PAGE_SIZE;
+      const posts = await fetchPosts({ from, to });
+      return posts;
+    },
+    
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => {
+      if (lastPage.length < PAGE_SIZE) return undefined;
+
+      return allPages.length;
+    },
+  });
+}
